c671272ddc7e2a234052b6181d0b126444f9871b
Matthew Steele <matthewsteeleonline@gmail.com>
2020-08-06 03:46:54 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -72,8 +72,8 @@ class Person
           person.happiness -= 2
           p "blah blah partisan blah lobbyist"
         elsif (topic == "weather")
-          self.happiness += 2 
-          person.happiness += 2
+          self.happiness += 1  
+          person.happiness += 1 
           p "blah blah sun blah rain"
         else topic
       end
