62cbc1ab93d98ba9a7372555fab678cc7d2b0d7e
Matthew Steele <matthewsteeleonline@gmail.com>
2020-08-06 03:04:10 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,24 @@
 # your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness
+    
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+  end
+  
+  def self.name
+    @name = name
+  end
+  
+  def self.happiness
+      if @happiness <= 10
+    @happiness = happiness
+      else 
+        @happiness = 10
+      end
+  end
+
+end
