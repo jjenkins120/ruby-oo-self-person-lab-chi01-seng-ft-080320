1e4da5148e0db62215c6127cd55da8efc51e8e53
Matthew Steele <matthewsteeleonline@gmail.com>
2020-08-06 03:44:15 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -68,7 +68,7 @@ class Person
     
     def start_conversation(person, topic)
       if (topic == "politics")
-        "blah blah partisan blah lobbyist"
+        p "blah blah partisan blah lobbyist"
         self.happiness -= (2)
         person.happiness -= (2)
       else topic
